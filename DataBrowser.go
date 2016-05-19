d4fab971929da16069bbc2704789093d65e760f8
darian.hickman <darian.hickman@villagethegame.com>
2016-05-19 12:38:37 -0400

    Pulls file from server url into tempfile and runs same table writer as local.

--- DataBrowser.go
+++ DataBrowser.go
@@ -3,16 +3,28 @@ package main
 import (
 	_ "database/sql"
 	_ "encoding/csv"
-	_ "fmt"
+	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/olekukonko/tablewriter"
 	"github.com/tealeg/xlsx"
-	_ "io/ioutil"
+	"io"
 	"log"
+	"net/http"
 	"os"
+	"strings"
 )
 
+//@TODO  Learn better way to handle value, error returns when you just want to return error.
+
 var logger = log.New(os.Stderr, "DB ", 0)
+
+func main() {
+	if strings.Contains(os.Args[1], "://") {
+		ViewServerFile(os.Args[1])
+	} else {
+		ViewLocalFile(os.Args[1])
+	}
+}
 
 func ViewLocalFile(filepath string) {
 	xlFile, err := xlsx.OpenFile(filepath)
@@ -43,12 +55,40 @@ func ViewLocalFile(filepath string) {
 }
 
 func ViewServerFile(urlpath string) {
-
+	tempfilepath, err := downloadFromUrl(urlpath)
+	checkError(err)
+	ViewLocalFile(tempfilepath)
 }
 
-func main() {
-	ViewLocalFile(os.Args[1])
+func downloadFromUrl(url string) (string, error) {
+	tokens := strings.Split(url, "/")
+	fileName := tokens[len(tokens)-1]
+	fmt.Println("Downloading", url, "to", fileName)
 
+	// TODO: check file existence first with io.IsExist
+	output, err := os.Create(fileName)
+	if err != nil {
+		fmt.Println("Error while creating", fileName, "-", err)
+		return "", err
+	}
+	defer output.Close()
+
+	response, err := http.Get(url)
+
+	if err != nil {
+		fmt.Println("Error while downloading", url, "-", err)
+		return "", err
+	}
+	defer response.Body.Close()
+
+	n, err := io.Copy(output, response.Body)
+	if err != nil {
+		fmt.Println("Error while downloading", url, "-", err)
+		return "", err
+	}
+
+	fmt.Println(n, "bytes downloaded.")
+	return fileName, nil
 }
 
 func checkError(err error) {
